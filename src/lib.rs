130a9578cfe03027c22500ff4d0ef95b519c2bd4
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-05 07:41:40 +0400

    Add argument to endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-async fn subscribe() -> HttpResponse {
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
