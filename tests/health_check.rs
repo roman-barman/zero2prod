f34ea0422161023909834f2121a03f6fb0e902a2
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-12 09:31:02 +0400

    Add secrecy crate

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use secrecy::ExposeSecret;
+use secrecy::Secret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use std::sync::LazyLock;
@@ -123,20 +125,21 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let maintenance_settings = DatabaseSettings {
         database_name: "postgres".to_string(),
         username: "postgres".to_string(),
-        password: "password".to_string(),
+        password: Secret::new("password".to_string()),
         ..config.clone()
     };
 
-    let mut connection = PgConnection::connect(&maintenance_settings.get_connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&maintenance_settings.get_connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.get_connection_string())
+    let connection_pool = PgPool::connect(&config.get_connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
