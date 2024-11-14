31747b99d2f9853569f1225d6b3d0760ee3d9737
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-14 20:52:58 +0400

    Update configuration

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,14 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
+    let connection_pool = PgPool::connect_lazy(
         configuration
             .database
             .get_connection_string()
             .expose_secret(),
     )
-    .await
     .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = configuration.application.get_application_address();
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
