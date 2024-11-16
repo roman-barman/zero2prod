3e44740a15c0c32f1eaf90289466a18329eb9424
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-16 17:34:21 +0400

    Add tests for name

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,9 +19,14 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
@@ -45,7 +50,7 @@ async fn insert_subscriber(
         "#,
         Uuid::new_v4(),
         new_subscriber.email,
-        new_subscriber.name.inner_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
