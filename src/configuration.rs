c5d278b9af6e0f13f8a3ac7a5803338bfd3207e2
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-21 17:19:27 +0400

    Refactor email client tests

--- src/configuration.rs
+++ src/configuration.rs
@@ -34,6 +34,7 @@ pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
     pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
 }
 
 pub enum Environment {
@@ -121,5 +122,9 @@ impl DatabaseSettings {
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
