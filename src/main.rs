2f08e0d3a0574033d72117d0604fcad7351142d5
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-22 10:59:02 +0400

    Create build function for server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::build;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -11,20 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-    let connection_pool =
-        PgPoolOptions::new().connect_lazy_with(configuration.database.get_connection_options());
-    let address = configuration.application.get_application_address();
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
