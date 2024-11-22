083a6fbd8feaef86d6537ca89578344cd0ef30ef
Roman Zanko <roman.zanko.vl@gmail.com>
2024-11-22 10:59:02 +0400

    Create build function for server

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,35 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool =
+        PgPoolOptions::new().connect_lazy_with(configuration.database.get_connection_options());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = configuration.application.get_application_address();
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
