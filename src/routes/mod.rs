bacf69814c146cbf1585c75cf6d2e17f063cc15e
Roman Zanko <roman.zanko.vl@gmail.com>
2024-12-05 10:26:25 +0400

    Add home page

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,12 @@
 mod health_check;
 mod helpers;
+mod home;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use health_check::*;
+pub use home::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
