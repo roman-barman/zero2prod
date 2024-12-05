c7e3fb475a39992fac67176865969a5f7c2d19e7
Roman Zanko <roman.zanko.vl@gmail.com>
2024-12-05 10:58:22 +0400

    Add login page

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,14 @@
 mod health_check;
 mod helpers;
 mod home;
+mod login;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use health_check::*;
 pub use home::*;
+pub use login::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
