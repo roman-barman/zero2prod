3edfae4d4709ce2c26ba3f604fc45956ba643827
Roman Zanko <roman.zanko.vl@gmail.com>
2024-12-06 09:47:53 +0400

    Extract authentication module

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,13 @@
 use actix_web::http::header::LOCATION;
+use actix_web::web;
 use actix_web::HttpResponse;
+use secrecy::Secret;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
 
 pub async fn login() -> HttpResponse {
     HttpResponse::SeeOther()
