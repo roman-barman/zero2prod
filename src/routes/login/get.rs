e99e253a0af852f2a3b7b663e80ab2d02fa22d44
Roman Zanko <roman.zanko.vl@gmail.com>
2025-01-06 10:53:58 +0400

    Add flash messages

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,17 +1,15 @@
-use actix_web::cookie::{time::Duration, Cookie};
 use actix_web::http::header::ContentType;
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::HttpResponse;
+use actix_web_flash_messages::{IncomingFlashMessages, Level};
+use std::fmt::Write;
 
-pub async fn login_form(request: HttpRequest) -> HttpResponse {
-    let error_html = match request.cookie("_flash") {
-        None => "".into(),
-        Some(cookie) => {
-            format!("<p><i>{}</i></p>", cookie.value())
-        }
-    };
+pub async fn login_form(flash_message: IncomingFlashMessages) -> HttpResponse {
+    let mut error_html = String::new();
+    for m in flash_message.iter().filter(|m| m.level() == Level::Error) {
+        writeln!(error_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"
             <!DOCTYPE html>
