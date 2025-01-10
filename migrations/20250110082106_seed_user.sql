f56d11bcd5ceb70bd3f40938d98d1f9fdea1d641
Roman Zanko <roman.zanko.vl@gmail.com>
2025-01-10 12:36:07 +0400

    Add admin user

---
+++ migrations/20250110082106_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'ddf8994f-d522-4659-8d02-c1d479057be6',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$pvcngHPAHqLBsGKTt2EfPQ$orOfb0Wq3bHaIV0sJaZER7gdnoVeLn0J47hTuNzaHmw'
+);
